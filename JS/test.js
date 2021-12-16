2d5c1407ee11d4f1f1a99ac892616b0b83284fae
annalinsmedes <153046@csgaugustinus.nl>
2021-12-16 14:19:24 +0000

    helloooo

--- JS/test.js
+++ JS/test.js
@@ -1,23 +1,24 @@
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
+  canvas = createCanvas(600,300);
   canvas.parent('processing');
-  //noLoop();
-}
-
-function tekenkerk(hoogte) {
-noStroke();
-fill('black');
-rect(0,350,75,hoogte);
-fill('brown');
-triangle(0,350,75,350,37,310);
 }
 
 function draw() {
   noStroke();
-  tekenkerk(300);
-  translate(200,-100);
-  tekenkerk(300);
-  translate(100,-100);
-  tekenkerk(300);
+  background('cyan');
+
+  if (mouseY > 200) {
+    fill('orange')
+  }
+  else {
+  fill('yellow');
+  }
+  ellipse(width/2,200,200);
+  fill('green');
+  rect(0,200,600);
+
+  fill('red');
+  triangle(90, 100, 50, 150, 130, 150)
+  fill('gray');
+  rect(50, 150, 80, 80);
 }
